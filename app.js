e8cf2ad57ab199943eacb61a2a0c61e51ed5729d
scofield1906 <13058004410@163.com>
2020-10-23 12:06:25 +0800

    10.23_4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const bodyParser=require('body-parser');
-const data=require('./routes/data.js');
+const luyou=require('./routes/luyou.js');
 
 var app=express();
 
@@ -11,4 +11,4 @@ app.use(bodyParser.urlencoded({
 	extended:false
 }));
 
-app.use('/data',data);
+app.use('/luyou',luyou);
